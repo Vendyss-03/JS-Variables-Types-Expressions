6a92c6ce90df4af58bb801ba4a2223029eebcccc
Vendyss-03 <64092106+Vendyss-03@users.noreply.github.com>
2020-04-26 20:23:12 +0200

    Finished exercises in JS

---
+++ scripts.js
@@ -0,0 +1,47 @@
+//Part one//
+
+let age = 2020 - 1995;
+console.log(age);
+
+let ww2 = 1945 - 1939;
+console.log(ww2);
+
+let tesla = 725.15 * 76;
+console.log(tesla);
+
+let secondsOfDay = 24 * 60 * 60;
+console.log(secondsOfDay);
+
+let hungarians = (9773000 / 7800000000) * 100;
+console.log(hungarians);
+
+let bigger = 9600000 / 78865;
+console.log(bigger);
+
+let participants = 654 % 7;
+console.log(participants);
+
+//Part two//
+
+let names = ['Felix', 'Dominik', 'Eliška', 'Tadeáš', 'Jonáš'];
+console.log(names);
+
+let trueOrFalse = names[2].length > 5;
+console.log(trueOrFalse);
+
+names.push = 'Berenika';
+console.log(names);
+
+names[0] = 'Vendy';
+console.log(names);
+
+let moreThan4 = names.length > 4;
+console.log(moreThan4);
+
+//Part three//
+
+let palette = {
+    colors: ['Aquamarine', 'Violet', 'Navy', 'Black', 'Yellow']
+    };
+    palette.hasManyColors = palette.colors.length > 3;
+console.log(palette);
